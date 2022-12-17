64c4eb565fd8e58d506c59e2fc69664f44f5c5ed
acheung <adrian.che0222@gmail.com>
2022-12-17 11:13:47 +1100

    day9 part 2

--- 2022/day9/Program.cs
+++ 2022/day9/Program.cs
@@ -1,52 +1,86 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+const int NUM_KNOTS = 10; // 2 for part 1
+
 string[] lines = File.ReadAllLines("input.txt");
 
-Tuple<int, int> headPosition = Tuple.Create<int, int>(0, 0);
-Tuple<int, int> tailPosition = Tuple.Create<int, int>(0, 0);
-Dictionary<char, Tuple<int, int>> unitVectors = new Dictionary<char, Tuple<int, int>>
-{
-    {'R', Tuple.Create<int, int>(1, 0)},
-    {'L', Tuple.Create<int, int>(-1, 0)},
-    {'U', Tuple.Create<int, int>(0, 1)},
-    {'D', Tuple.Create<int, int>(0, -1)}
-};
-
-HashSet<Tuple<int, int>> tailVisited = new HashSet<Tuple<int, int>> { Tuple.Create<int, int>(0, 0) };
-
+Rope rope = new Rope(NUM_KNOTS);
 foreach(string line in lines)
 {
     char direction = line.Split(" ")[0][0];
     int distance = Convert.ToInt32(line.Split(" ")[1]);
-    for (int i = 0; i < distance; i++)
-    {
-        Tuple<int, int> u = unitVectors[direction];
-        headPosition = Tuple.Create<int, int>(headPosition.Item1 + u.Item1, headPosition.Item2 + u.Item2);
-        AdjustTail();
-    }
+    rope.MoveHead(direction, distance);
 }
 
-void AdjustTail()
-{
-    Tuple<int, int> delta = Tuple.Create<int, int> (headPosition.Item1 - tailPosition.Item1, 
-        headPosition.Item2 - tailPosition.Item2);
-    if (Math.Abs(delta.Item1) > 1 || Math.Abs(delta.Item2) > 1)
-    {
-        int deltaX = delta.Item1 == 0 ? 0 : (int)(delta.Item1/Math.Abs(delta.Item1));
-        int deltaY = delta.Item2 == 0 ? 0 : (int)(delta.Item2/Math.Abs(delta.Item2));
-
-        Tuple<int, int> newTailPosition = Tuple.Create<int, int> (
-            tailPosition.Item1 + deltaX,
-            tailPosition.Item2 + deltaY
-        );
-        if (!tailVisited.Contains(newTailPosition))
-        {
-            tailVisited.Add(newTailPosition);
-        }
-        tailPosition = newTailPosition;
-    }
-}
-
-Console.WriteLine(tailVisited.Count());
+Console.WriteLine($"Tail visited {rope.GetNumberOfPositionsVisitedByKnot(NUM_KNOTS - 1)} positions");
 Console.WriteLine("done");
+
+public class Rope
+{
+    List<HashSet<Vector2>> _positionsVisited = new List<HashSet<Vector2>>();
+    private List<Vector2> _knots = new List<Vector2>();
+
+    public Rope (int numKnots)
+    {
+        for (int i = 0; i < numKnots; i++)
+        {
+            _knots.Add(Vector2.Zero);
+            _positionsVisited.Add(new HashSet<Vector2> {Vector2.Zero});
+        }
+    }
+
+    public int GetNumberOfPositionsVisitedByKnot(int knotId)
+    {
+        return _positionsVisited[knotId].Count();
+    }
+
+    public void MoveHead(char direction, int distance)
+    {
+        for (int i = 0; i < distance; i++)
+        {
+            Vector2 newHead = _knots[0];
+            switch (direction)
+            {
+                case 'R':
+                    newHead = _knots[0] + Vector2.UnitX;
+                    break;
+                case 'L':
+                    newHead = _knots[0] + (-1*Vector2.UnitX);
+                    break;
+                case 'U':
+                    newHead = _knots[0] + Vector2.UnitY;
+                    break;
+                case 'D':
+                    newHead = _knots[0] + (-1*Vector2.UnitY);
+                    break;
+            }
+            _positionsVisited[0].Add(newHead);
+            _knots[0] = newHead;
+            for (int knotId = 1; knotId < _knots.Count; knotId++)
+            {
+                AdjustKnot(knotId);
+            }
+        }
+    }
+
+    private void AdjustKnot(int knotId)
+    {
+        Vector2 knot = _knots[knotId];
+        Vector2 knotInFront = _knots[knotId -1];
+        Vector2 delta = knotInFront - knot;
+        if (Math.Abs(delta.X) > 1 || Math.Abs(delta.Y) > 1)
+        {
+            int deltaX = delta.X == 0 ? 0 : (int)(delta.X/Math.Abs(delta.X));
+            int deltaY = delta.Y == 0 ? 0 : (int)(delta.Y/Math.Abs(delta.Y));
+            Vector2 moveDelta = new Vector2(deltaX, deltaY);
+            Vector2 newKnotPosition = knot + moveDelta;
+            _positionsVisited[knotId].Add(newKnotPosition);
+            _knots[knotId] = newKnotPosition;
+        }
+    }
+}
+
+
 
 
