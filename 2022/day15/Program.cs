772e38cb160082cb1e7b83575d46c4f8042aef29
acheung <adrian.che0222@gmail.com>
2023-01-02 11:06:35 +1100

    day 15 part 2

--- 2022/day15/Program.cs
+++ 2022/day15/Program.cs
@@ -1,11 +1,15 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
 
 // See https://aka.ms/new-console-template for more information
 string[] lines = File.ReadAllLines("input.txt");
 List<SensorBeaconPair> sensorBeaconPairs = new List<SensorBeaconPair>();
-List<(int, int)> intersects = new List<(int, int)>();
+List<List<(int, int)>> intersects = new List<List<(int, int)>>();
+List<List<(int, int)>> simplifiedIntervals = new List<List<(int, int)>>();
 HashSet<int> noBeacons = new HashSet<int>();
-int y0 = 2000000;
+//int y0 = 2000000;
+int yMin = 0;
+int yMax = 4000000;
 foreach (string line in lines)
 {
     Regex rx = new Regex(@"Sensor at x=(-?\d*), y=(-?\d*): closest beacon is at x=(-?\d*), y=(-?\d*)", 
@@ -22,27 +26,74 @@ foreach (string line in lines)
         BeaconY = beaconY
     };
     sensorBeaconPairs.Add(pair);
-    if (pair.ManhattanDistance >= Math.Abs(pair.SensorY - y0))
+    for (int y0 = yMin; y0 <= yMax; y0++)
     {
-        intersects.Add(ComputeIntersect(pair, y0));
-    }
-}
-
-foreach ((int, int) interval  in intersects)
-{
-    (int low, int high) = interval;
-    for (int i = low; i <= high; i++)
-    {
-        if (!sensorBeaconPairs.Any(p => p.BeaconX == i && p.BeaconY == y0))
+        if (intersects.Count < y0+1) 
         {
-            noBeacons.Add(i);
+            intersects.Add(new List<(int, int)>());
+        }
+        if (pair.ManhattanDistance >= Math.Abs(pair.SensorY - y0))
+        {
+            intersects[y0].Add(ComputeIntersect(pair, y0));
         }
     }
 }
 
+foreach (List<(int, int)> intervals in intersects)
+{
+    simplifiedIntervals.Add(SimplifyIntervals(intervals));
+}
+
+// foreach ((int, int) interval  in intersects)
+// {
+//     (int low, int high) = interval;
+//     for (int i = low; i <= high; i++)
+//     {
+//         if (!sensorBeaconPairs.Any(p => p.BeaconX == i && p.BeaconY == y0))
+//         {
+//             noBeacons.Add(i);
+//         }
+//     }
+// }
+
+
+var yPos = simplifiedIntervals.FindIndex(si => si.Count > 1);
+BigInteger xPos = simplifiedIntervals[yPos][0].Item2 + 1;
+BigInteger tuningFrequency = xPos * 4000000 + yPos;
+
+Console.WriteLine($"Distress beacon is at x = {xPos}, y = {yPos}");
+Console.WriteLine($"Tuning Frequency is {tuningFrequency}");
 Console.WriteLine(noBeacons.Count);
 
 Console.WriteLine("done");
+
+List<(int, int)> SimplifyIntervals(List<(int, int)> intervals)
+{
+    List<(int, int)> simplified = new List<(int, int)>();
+    var sortedIntervals = intervals.OrderBy(iv => iv.Item1).ToList();
+    for (int i = 0; i < sortedIntervals.Count; i++)
+    {
+        var currentInterval = sortedIntervals[i];
+        while (i+1 < sortedIntervals.Count)
+        {
+            var nextInterval = sortedIntervals[i+1];
+            if (nextInterval.Item1 <= currentInterval.Item2 + 1)
+            {
+                if (nextInterval.Item2 >= currentInterval.Item2)
+                {
+                    currentInterval = (currentInterval.Item1, nextInterval.Item2);
+                }
+            }
+            else
+            {
+                break;
+            }
+            i++;
+        }
+        simplified.Add(currentInterval);
+    }
+    return simplified;
+}
 
 (int a, int b) ComputeIntersect(SensorBeaconPair pair, int y)
 {
